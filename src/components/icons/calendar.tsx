eec9640f0f5bd796547594a3c11d58c1f67eec24
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-04-19 23:19:14 +0200

    fix: svg attributes

--- src/components/icons/calendar.tsx
+++ src/components/icons/calendar.tsx
@@ -9,7 +9,7 @@ export function CalendarIcon() {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="square"
+        strokeLinecap="square"
         d="M9.5 5.5h-4v4m4-4h5m-5 0v-1m0 1v1m5-1h4v4m-4-4v-1m0 1v1m-9 3v9h13v-9m-13 0h13"
       ></path>
     </svg>
