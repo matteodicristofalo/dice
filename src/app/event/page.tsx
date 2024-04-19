0c6ddd27368d210fb4ee15c815a16857c2427023
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-04-19 21:26:20 +0200

    feat: add location and calendar icons

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.css";
 import { event } from "../../data";
 import { Button } from "@components/button/button";
+import { LocationIcon } from "@icons/location";
+import { CalendarIcon } from "@icons/calendar";
 
 export default function Event() {
   return (
@@ -12,9 +14,13 @@ export default function Event() {
 
         <div>
           <h1 className="is-title-xxl">{event.name}</h1>
-          <p className="is-body-l has-mt-8">{event.location}</p>
-          <p className="is-body-l has-mt-8 has-foreground-accent">
-            {event.date}
+          <p className="flex is-body-l has-gap-8 has-mt-24">
+            <LocationIcon />
+            <span>{event.location}</span>
+          </p>
+          <p className="flex is-body-l has-gap-8 has-mt-8">
+            <CalendarIcon />
+            <span className="has-foreground-accent">{event.date}</span>
           </p>
 
           <div className={styles["price"]}>
@@ -39,21 +45,21 @@ export default function Event() {
             <h2 className="is-title-l has-mt-64 has-mb-16">Line up</h2>
             <div>
               <div className={styles["artist"]}>
-                <div className={styles["artist__data"]}>
+                <div className="flex has-gap-16">
                   <div className={styles["artist__picture"]}></div>
                   <span>Mario Rossi</span>
                 </div>
                 <Button variant="foreground">Follow</Button>
               </div>
               <div className={styles["artist"]}>
-                <div className={styles["artist__data"]}>
+                <div className="flex has-gap-16">
                   <div className={styles["artist__picture"]}></div>
                   <span>Mario Rossi</span>
                 </div>
                 <Button variant="foreground">Follow</Button>
               </div>
               <div className={styles["artist"]}>
-                <div className={styles["artist__data"]}>
+                <div className="flex has-gap-16">
                   <div className={styles["artist__picture"]}></div>
                   <span>Mario Rossi</span>
                 </div>
---
+++ src/components/icons/calendar.tsx
@@ -0,0 +1,17 @@
+export function CalendarIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="square"
+        d="M9.5 5.5h-4v4m4-4h5m-5 0v-1m0 1v1m5-1h4v4m-4-4v-1m0 1v1m-9 3v9h13v-9m-13 0h13"
+      ></path>
+    </svg>
+  );
+}
