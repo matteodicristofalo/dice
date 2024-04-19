e8f0eeb0dfdf808535a9004c8b2424fea243f105
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-04-19 12:29:46 +0200

    refactor: move mocked data into a separate module

---
+++ src/data.ts
@@ -0,0 +1,10 @@
+export const event = {
+  name: "Ciao Discoteca Italiana. Notturno Italiano",
+  date: "dom 21 Maggio",
+  location: "Stecca 3.0",
+  price: "$46,04",
+  poster:
+    "https://dice-media.imgix.net/attachments/2024-04-10/9d07fc2a-48f3-4efe-bd8e-1de1866bba99.jpg?rect=0%2C0%2C1080%2C1080&auto=format%2Ccompress&q=40&w=328&fit=max&dpr=2",
+};
+
+export const events = Array(8).fill(event);
