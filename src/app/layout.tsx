d9eb047ed22726a3fa6953c47da2db68f9d8807d
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-04-24 19:55:22 +0200

    fix: the way the fonts are imported

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import { Header } from "@components/header/header";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import "./typography.css";
 import "./spacings.css";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  style: "normal",
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={dmSans.className}>
       <body>
         <Header />
         {children}
