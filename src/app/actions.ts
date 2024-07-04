5025b344f52e706d89e1316e16e783c10b34d53e
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-07-04 18:08:43 +0200

    Revert "temporary remove fetch revalidation"
    This reverts commit f83070db678da2075df04608242eca32cee5b1a0.

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -95,8 +95,8 @@ async function fetchContentful(graphQlQuery: string) {
       Authorization: `Bearer ${accessToken}`,
     },
     body: JSON.stringify(query),
-    // next: {
-    //   revalidate: 60,
-    // },
+    next: {
+      revalidate: 60,
+    },
   });
 }
