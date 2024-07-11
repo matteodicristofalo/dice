dda5f5afefe7141babc38385c9cc370e117542e8
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-07-11 21:03:46 +0200

    add drawer's close button

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CloseIcon } from "@components/icons/close";
 import { useDrawer } from "./drawer-context";
 import styles from "./drawer.module.scss";
 
@@ -8,10 +9,17 @@ interface DrawerProps {
 }
 
 export function Drawer({ children }: DrawerProps) {
-  const { state } = useDrawer();
+  const { state, close } = useDrawer();
 
   return (
     <div className={`${styles["drawer"]} ${styles[`drawer-${state}`]}`}>
+      <button
+        type="button"
+        className={styles["drawer__close-button"]}
+        onClick={close}
+      >
+        <CloseIcon />
+      </button>
       <div className={styles["drawer__content"]}>{children}</div>
     </div>
   );
---
+++ src/components/icons/close.tsx
@@ -0,0 +1,18 @@
+export function CloseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="square"
+        stroke-width="1.25"
+        d="m16.5 7.5-9 9m0-9 9 9"
+      ></path>
+    </svg>
+  );
+}
