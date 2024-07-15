3f51398642bb6ddda632abdfb1b64b90abb0a669
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2024-07-15 21:48:45 +0200

    add mixin for desktop's media query

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className={styles["container"]}>
       <h1 className={styles["heading"]}>Page not found</h1>
       <p className={styles["paragraph"]}>
-        We are sorry! this page has not been implemented yet...
+        We are sorry! this page has not been implemented yet
       </p>
       <Link href="/">
         <Button variant="primary">Return Home</Button>
